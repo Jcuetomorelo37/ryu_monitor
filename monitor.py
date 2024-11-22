68674fd2199c43388c0a241521592c2120f66590
Tu Nombre <you@example.com>
2024-11-22 11:23:52 -0500

    :sparkles: Starting project...

---
+++ monitor.py
@@ -0,0 +1,149 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib import hub
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+import psutil
+from flask import Flask, jsonify
+import threading
+import time
+
+
+class TrafficMonitor(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(TrafficMonitor, self).__init__(*args, **kwargs)
+        self.datapaths = {}
+        self.mac_to_port = {}
+        self.monitor_thread = hub.spawn(self._monitor)
+        self.metrics = {
+            "cpu": 0,
+            "memory": 0,
+            "switches": {},
+            "traffic": [],
+            "events": []
+        }
+        threading.Thread(target=self.start_http_server, daemon=True).start()
+
+    def start_http_server(self):
+        app = Flask(__name__)
+
+        @app.route('/metrics', methods=['GET'])
+        def get_metrics():
+            return jsonify(self.metrics)
+
+        app.run(host='0.0.0.0', port=5000)
+
+    def _monitor(self):
+        while True:
+            for dp in self.datapaths.values():
+                self._request_stats(dp)
+            self._log_system_stats()
+            hub.sleep(10)
+
+    def _log_system_stats(self):
+        cpu_usage = psutil.cpu_percent()
+        memory_info = psutil.virtual_memory()
+        self.metrics["cpu"] = cpu_usage
+        self.metrics["memory"] = memory_info.percent
+        self.logger.info('CPU Usage: %s%%', cpu_usage)
+        self.logger.info('Memory Usage: %s%% (%s MB used)', memory_info.percent, memory_info.used / (1024 ** 2))
+
+    def _request_stats(self, datapath):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def port_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        switch_id = ev.msg.datapath.id
+        self.metrics["switches"].setdefault(switch_id, {})
+
+        for stat in sorted(body, key=lambda x: x.port_no):
+            self.logger.info(
+                'Switch ID=%s Port=%d: RX packets=%d TX packets=%d RX bytes=%d TX bytes=%d',
+                switch_id, stat.port_no, stat.rx_packets, stat.tx_packets, stat.rx_bytes, stat.tx_bytes
+            )
+            self.metrics["switches"][switch_id][stat.port_no] = {
+                "rx_packets": stat.rx_packets,
+                "tx_packets": stat.tx_packets,
+                "rx_bytes": stat.rx_bytes,
+                "tx_bytes": stat.tx_bytes,
+            }
+
+    @set_ev_cls(ofp_event.EventOFPStateChange, MAIN_DISPATCHER)
+    def _state_change_handler(self, ev):
+        datapath = ev.datapath
+        timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        if ev.state == MAIN_DISPATCHER:
+            if datapath.id not in self.datapaths:
+                self.datapaths[datapath.id] = datapath
+                self.logger.info("Switch %s conectado", datapath.id)
+                self.metrics["events"].append(
+                    {"timestamp": timestamp, "event": f"Switch {datapath.id} conectado"}
+                )
+        elif ev.state == 'DEAD_DISPATCHER':
+            if datapath.id in self.datapaths:
+                del self.datapaths[datapath.id]
+                self.logger.info("Switch %s desconectado", datapath.id)
+                self.metrics["events"].append(
+                    {"timestamp": timestamp, "event": f"Switch {datapath.id} desconectado"}
+                )
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        """Maneja paquetes que no coinciden con ningún flujo en el switch."""
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            return
+
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        if out_port != ofproto.OFPP_FLOOD:
+            actions = [parser.OFPActionOutput(out_port)]
+            match = parser.OFPMatch(in_port=in_port, eth_src=src, eth_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+
+        actions = [parser.OFPActionOutput(out_port)]
+        out = parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
+            actions=actions, data=msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
+        )
+        datapath.send_msg(out)
+
+    def add_flow(self, datapath, priority, match, actions):
+        """Agrega un flujo a un switch."""
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(
+            datapath=datapath, priority=priority, match=match, instructions=inst
+        )
+        datapath.send_msg(mod)
